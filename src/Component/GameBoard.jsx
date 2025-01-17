0920973dc4553c05da1078e8c374e31df760c426
Satyam Bheel <136482276+satyam-bheel1999@users.noreply.github.com>
2025-01-17 17:48:55 +0530

    Update GameBoard.jsx

--- src/Component/GameBoard.jsx
+++ src/Component/GameBoard.jsx
@@ -3,16 +3,23 @@ import React, { useState } from 'react'
 function GameBoard() {
 
     const [board, setBoard] = useState(Array(9).fill(null));
+    const [next, setNext] = useState(true);
 
-    console.log(board);
+    const handleClisk = (index) =>{
+        const newBoard = board.slice();
+        newBoard[index] = next? 'x' : 'y';
 
+        setBoard(newBoard);
+        console.log(board);
+        setNext(!next);
+    }
 
   return (
       <div>
           <div className='grid grid-cols-3 gap-1 w-40 h-48'>
               {
                   board.map((cell, index) => (
-                      <button className='border border-black'>{cell}</button>))
+                      <button key={index} className='border border-black text-lg' onClick={handleClisk}>{cell}</button>))
               }
 
           </div>
